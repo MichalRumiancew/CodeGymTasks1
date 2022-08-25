4538de5472760492f5b1453aa3b0a6891055653c
MichalRumiancew <michalr11@gmail.com>
2022-08-25 19:02:57 +0200

    task0309 sum 5 first numbers passed

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0309/Solution.java
@@ -0,0 +1,17 @@
+package pl.codegym.task.task03.task0309;
+
+/* 
+Suma 5 liczb
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+   int sum = 0;
+        for (int i = 1; i <=5 ; i++) {
+            sum = sum + i;
+            System.out.println(sum);
+        }
+
+    }
+}
+
