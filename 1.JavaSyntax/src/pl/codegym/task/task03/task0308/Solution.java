979edb5588794051d7cccc5c5b9c1d2d8eebe9b4
MichalRumiancew <michalr11@gmail.com>
2022-08-25 18:53:00 +0200

    task0308 passed

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0308/Solution.java
@@ -0,0 +1,29 @@
+package pl.codegym.task.task03.task0308;
+
+/* 
+Iloczyn 10 liczb
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int iloczyn10 = 1*2*3*4*5*6*7*8*9*10;
+        System.out.println(iloczyn10);
+    }
+}
+
+/*
+Iloczyn 10 liczb
+        Wyświetl iloczyn 10 liczb od 1 do 10.
+        Wynik to jedna liczba.
+
+        Wskazówka:
+        będzie to ponad trzy miliony.
+
+
+        Requirements:
+        1. Program musi wyświetlać liczbę całkowitą.
+        2. Metoda main powinna wywoływać metodę System.out.println.
+        3. Wyświetlona liczba powinna być większa niż trzy miliony.
+        4. Wyświetlona liczba musi spełniać warunki zadania.
+
+ */
