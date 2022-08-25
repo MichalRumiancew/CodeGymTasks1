ce019aef1fb4223ea9230780d2924a438dd4dd95
MichalRumiancew <michalr11@gmail.com>
2022-08-25 18:33:16 +0200

    task0306 passed

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0306/Solution.java
@@ -0,0 +1,24 @@
+package pl.codegym.task.task03.task0306;
+
+/* 
+Łamigłówka w nawiasach
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(2 * (3 + 4 * (5 + 6 * 7)));
+    }
+}
+
+/*
+Łamigłówka w nawiasach
+Ustaw poprawnie (inaczej) nawiasy, aby wyświetlić na ekranie liczbę 382.
+
+
+Requirements:
+1. Program nie może wczytywać danych z klawiatury.
+2. Metoda main musi zawierać tylko jedno wywołanie System.out.println.
+3. Nie można zmieniać kolejności cyfr i operacji arytmetycznych.
+4. Ostateczne wyrażenie powinno mieć 2 nawiasy otwierające i 2 nawiasy zamykające.
+5. Metoda main powinna wyświetlać liczbę 382.
+ */
