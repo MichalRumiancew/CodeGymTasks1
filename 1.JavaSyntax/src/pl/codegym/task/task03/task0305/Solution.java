8742a93405dc311869e8e1fbdda316499e3a06d9
MichalRumiancew <michalr11@gmail.com>
2022-08-25 18:10:09 +0200

    task0305 passed

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0305/Solution.java
@@ -0,0 +1,22 @@
+package pl.codegym.task.task03.task0305;
+
+/* 
+Czerwona zaraza
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("GRUDZIEŃ " + "31 " + "1982");
+    }
+}
+/*
+    Wyświetl swoją datę urodzenia w formacie:
+        MAJ 1 2012
+
+
+        Requirements:
+        1. Wyświetlony tekst powinien zawierać miesiąc, dzień i rok oddzielone spacjami.
+        2. Nazwa miesiąca powinna być napisana po polsku, wielkimi literami.
+        3. Rok musi składać się z czterech cyfr i nie może być mniejszy niż 1900.
+        4. Data urodzin musi być wcześniejsza niż bieżąca data.
+        */
