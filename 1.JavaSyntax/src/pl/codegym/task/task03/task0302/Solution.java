2fd54c835383c39546bd04aa7cef62ab948cbd84
MichalRumiancew <michalr11@gmail.com>
2022-08-25 16:18:54 +0200

    first commit a

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0302/Solution.java
@@ -0,0 +1,18 @@
+package pl.codegym.task.task03.task0302;
+
+/* 
+Wyświetl od razu
+*/
+
+public class Solution {
+    // tutaj wpisz swój kod
+
+    public static void main(String[] args) {
+        printString("Cześć, Amigo!");
+    }
+    public static void printString(String s){
+        System.out.println(s);
+    }
+}
+
+test;
