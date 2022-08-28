dcd9f4d3873f9ecd0b3096fc7560b95a123828a5
MichalRumiancew <michalr11@gmail.com>
2022-08-28 20:30:00 +0200

    task0313

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0313/Solution.java
@@ -0,0 +1,37 @@
+package pl.codegym.task.task03.task0313;
+
+/* 
+Ala ma kota
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = "Ala";
+        String b = "ma";
+        String c = "kota";
+
+        System.out.println(a+b+c);
+        System.out.println(a+c+b);
+        System.out.println(b+c+a);
+        System.out.println(b+a+c);
+        System.out.println(c+a+b);
+        System.out.println(c+b+a);
+
+    }
+}
+
+/*
+rezwiązanie code gym:
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Alamakota");
+        System.out.println("Alakotama");
+        System.out.println("maAlakota");
+        System.out.println("makotaAla");
+        System.out.println("kotaAlama");
+        System.out.println("kotamaAla");
+    }
+}
+
+
+ */
