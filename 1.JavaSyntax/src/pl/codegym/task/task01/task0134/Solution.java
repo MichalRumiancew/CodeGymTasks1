465b6679c5c92f135a49c0e3786c4917c6fc0c36
MichalRumiancew <michalr11@gmail.com>
2022-08-28 18:55:35 +0200

    task0311 String concatenation

--- 1.JavaSyntax/src/pl/codegym/task/task01/task0134/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task01/task0134/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
     public static long getObjetosc(int a, int b, int c) {
         long result = a * b * c;
         return result * 1000;
-        //   rozwiązanie inne 1000L L-od razu zamieni na long
+        //   rozwiązanie inne 1000L L-od razu zamieni na long * 1000 bo w litrach
         //   public static long getObjetosc(int a, int b, int c) {
         //        return 1000L * a * b * c;
     }
