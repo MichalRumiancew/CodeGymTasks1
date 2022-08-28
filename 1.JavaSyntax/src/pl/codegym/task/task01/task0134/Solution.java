781619e0abdeadfea72f74b815b99cfa7ea92641
MichalRumiancew <michalr11@gmail.com>
2022-08-28 12:52:12 +0200

    task0134 int to Long

---
+++ 1.JavaSyntax/src/pl/codegym/task/task01/task0134/Solution.java
@@ -0,0 +1,19 @@
+package pl.codegym.task.task01.task0134;
+
+/* 
+Napełnij basen wodą
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjetosc(25, 5, 2));
+    }
+
+    public static long getObjetosc(int a, int b, int c) {
+        long result = a * b * c;
+        return result * 1000;
+        //   rozwiązanie inne 1000L L-od razu zamieni na long
+        //   public static long getObjetosc(int a, int b, int c) {
+        //        return 1000L * a * b * c;
+    }
+}
