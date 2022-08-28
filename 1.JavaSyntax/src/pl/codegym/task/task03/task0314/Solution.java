d308f744ea380c9ced8d5d3fa5add4e319027183
MichalRumiancew <michalr11@gmail.com>
2022-08-28 20:35:53 +0200

    task0314 multiplication table

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0314/Solution.java
@@ -0,0 +1,17 @@
+package pl.codegym.task.task03.task0314;
+
+/* 
+Tabliczka mnożenia
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            for (int j = 1; j <=10 ; j++) {
+                System.out.print(i*j);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
