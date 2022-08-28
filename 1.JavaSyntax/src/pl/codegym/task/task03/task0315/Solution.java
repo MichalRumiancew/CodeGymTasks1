ad7c7e5db530e9ee5df48b59c2018eec198229dd
MichalRumiancew <michalr11@gmail.com>
2022-08-28 20:41:06 +0200

    task0315 rainbow

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0315/Solution.java
@@ -0,0 +1,59 @@
+package pl.codegym.task.task03.task0315;
+
+/* 
+Czemu Patrzysz, Żabko Zielona...
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       new Czerwony();
+       new Pomaranczowy();
+       new Zolty();
+       new Zielony();
+       new Niebieski();
+       new Granatowy();
+       new Fioletowy();
+    }
+
+    public static class Czerwony {
+        public Czerwony() {
+            System.out.println("Czerwony");
+        }
+    }
+
+    public static class Pomaranczowy {
+        public Pomaranczowy() {
+            System.out.println("Pomarańczowy");
+        }
+    }
+
+    public static class Zolty {
+        public Zolty() {
+            System.out.println("Żółty");
+        }
+    }
+
+    public static class Zielony {
+        public Zielony() {
+            System.out.println("Zielony");
+        }
+    }
+
+    public static class Niebieski {
+        public Niebieski() {
+            System.out.println("Niebieski");
+        }
+    }
+
+    public static class Granatowy {
+        public Granatowy() {
+            System.out.println("Granatowy");
+        }
+    }
+
+    public static class Fioletowy {
+        public Fioletowy() {
+            System.out.println("Fioletowy");
+        }
+    }
+}
