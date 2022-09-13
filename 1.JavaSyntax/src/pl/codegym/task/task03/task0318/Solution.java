afbf7cab87282acce34e12ca43f49139500eb278
MichalRumiancew <michalr11@gmail.com>
2022-09-13 17:28:46 +0200

    task0318 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));                  String lat = reader.readLine();                  String imie = reader.readLine();

--- 1.JavaSyntax/src/pl/codegym/task/task03/task0318/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0318/Solution.java
@@ -1,13 +1,17 @@
 package pl.codegym.task.task03.task0318;
 
-/* 
+import java.io.*;
+
+/*
 Plan podboju świata
 */
 
-import java.io.*;
-
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //tutaj wpisz swój kod
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String lat = reader.readLine();
+        String imie = reader.readLine();
+
+        System.out.println(imie + " przejmie władzę nad światem za " + lat + " lat. Mua ha ha!");
     }
 }
