b7c38891f89738d7c8c10ac3a86d3ebe3cce3850
MichalRumiancew <michalr11@gmail.com>
2022-09-17 15:25:47 +0200

    fori

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0324/Solution.java
@@ -0,0 +1,14 @@
+package pl.codegym.task.task03.task0324;
+
+/* 
+Cele biznesowe
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Oczekuję wysokiej pensji, dlatego uczę się Javy");
+
+        }
+    }
+}
